859dede6daaef2e583a64b2fb4d5f567033ecb4f
Pieda6 <amiolemenseghe@gmail.com>
2026-06-03 20:02:18 +0100

    Add Home component with layout structure

---
+++ app/page.js
@@ -0,0 +1,25 @@
+import Navbar from '../components/Navbar'
+import Hero from '../components/Hero'
+import About from '../components/About'
+import Services from '../components/Services'
+import Market from '../components/Market'
+import Invest from '../components/Invest'
+import Angel from '../components/Angel'
+import Budget from '../components/Budget'
+import Footer from '../components/Footer'
+
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <About />
+      <Services />
+      <Market />
+      <Invest />
+      <Angel />
+      <Budget />
+      <Footer />
+    </>
+  )
+}
